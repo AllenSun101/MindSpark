293b320eac8227fcb1b19d92ef245a509a52bf0a
AllenSun101 <allensun21527@gmail.com>
2024-12-22 11:38:05 -0600

    Profile database fetch

--- frontend/app/Profile/page.js
+++ frontend/app/Profile/page.js
@@ -1,4 +1,40 @@
+'use client'
+
+import axios from "axios"
+import { useSession } from "next-auth/react"
+import { useState, useEffect } from "react";
 
 export default function Profile(){
     // fetch profile info, create document it not exist
+    const { data: session } = useSession();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+                const { data } = await axios.get("http://localhost:3001/get_profile", {
+                    params: {
+                        email: session.user.email,
+                        name: session.user.name,
+                    }
+                })
+            console.log(data);
+        }
+
+        if(session){
+            fetchProfile();
+        }
+    }, [session]);
+
+    if(!session?.user){
+        return(
+            <div>
+                <p>Sign In First!</p>
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <p>Profile</p>
+        </div>
+    )
 }
